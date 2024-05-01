b024a5991951725163bd551f3cc4c2e9d0f5241f
KIM0212DH <godqhr803@naver.com>
2024-05-01 23:34:17 +0900

    prepare test

--- javascriptEx/ex01/ex01.js
+++ javascriptEx/ex01/ex01.js
@@ -30,3 +30,18 @@ const coffeeP = {
 console.log(coffeeP.price);
 coffeeP.price = 5000;
 console.log(coffeeP.price)
+
+// Fetch API를 사용해 aaa.json 파일을 읽어옵니다.
+fetch('https://jwonchi.github.io/kopoclass/JSON/supJson.json')
+    .then(response => {
+        // JSON 형태로 응답을 변환합니다.
+        return response.json();
+    })
+    .then(data => {
+        // data가 1 이상인 경우 퀴즈 만드는 함수를 호출한다. 
+        console.log(data);
+
+    })
+    .catch(error => {
+        console.error('파일을 읽는 중 오류 발생:', error);
+    });
